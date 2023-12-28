e0e8a8c1b1fc9b57e43cfdae1e25e0c9af13705f
welton <welton.wmsoft@gmail.com>
2023-12-28 18:01:00 -0300

    criacao menu

--- sis03-ui/src/components/Navbar/index.js
+++ sis03-ui/src/components/Navbar/index.js
@@ -1,16 +1,17 @@
+import "./index.css"
 const Navbar = ()=> {
     return (
         <>
-        <div style={{display:"flex", width:"435px", marginLeft:"310px", marginTop:"25px"}}>
-           <h2 style={{width:"150px", background:"#BDB76B"}}>Usuario</h2>
-           <h2 style={{width:"150px", background:"#BDB76B"}}>Cliente</h2>
-           <h2 style={{width:"150px", background:"#BDB76B"}}>Produto</h2>
-           
-
+        <div style={{border:"10px solid#111", borderRadius:"0%"}}>
+          <ul>
+            <li><a href="#USUARIO">USUARIO</a></li>
+            <li><a href="#CLIENTE">CLIENTE</a></li>
+            <li><a href="PRODUTO">PRODUTO</a></li>
+            <li style={{float:"right"}}><a className = "active" href="SOBRE">SOBRE</a></li>
+          </ul>  
         </div>
-        
-        
-        
+          
+
         </>
         
         
